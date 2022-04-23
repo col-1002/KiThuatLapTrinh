29b841f7d50bd8a9784efd6764af706546ab2b5f
col-1002 <AT160725@actvn.edu.vn>
2022-04-24 00:45:51 +0700

    add 3

--- Subdomain-Scanner/sub-scanner.py
+++ Subdomain-Scanner/sub-scanner.py
@@ -1,2 +1,28 @@
-import request 
+import requests
+import sys
 
+list_domain = sys.argv[1]
+
+def domain_scanner(domain_name,sub_domnames):
+
+	print('-----------Scanner Started-----------'+"\n")
+		
+	for subdomain in sub_domnames:
+		url = f"http://{subdomain}.{domain_name}"
+		try:
+			requests.get(url)
+			print(f'[+] {url}')
+		except requests.ConnectionError:
+			pass
+			
+	print("\n"+'---------Scanning Finished---------')
+
+if __name__ == '__main__':
+	
+	dom_name = input("Enter the Domain Name:")
+	
+	with open(list_domain,'r') as file:
+		name = file.read()
+		sub_dom = name.splitlines()
+		
+	domain_scanner(dom_name,sub_dom)
